1bcd6a7dc072cccc4093003909afc38c8a01505e
Kam <kam.sportsdata@gmail.com>
2026-06-15 21:31:46 -0500

    Automate the Boring Stuff Notes

--- learning/python-basics/ATBS_v10.py
+++ learning/python-basics/ATBS_v10.py
@@ -2,17 +2,47 @@
 # Kam McIntire | Sports Analytics Journey
 # VIDEO 10: .
 # Source: Automate the Boring Stuff - Al Sweigart
-# Date: .
+# Date: 06/15/2026
 # =============================================================
 
 # CONCEPTS COVERED:
-#   .
+#   Global and Local Scopes
 
 # -------------------------------------------------------------
-# CONCEPT: .
+# CONCEPT: Global and Local Scopes
 # -------------------------------------------------------------
 
-# explanation and code here
+# Scopes are 'containers' of Variables (Global Variables & Local Variables)
+
+# Global Scope:
+name = Kam
+
+# Local Scope:
+this_file = "ATBS_v10.py"
+file_number = 10
+print("Enter this file's password below\nHint: (your name)_(file name)_(file number)")
+def password():
+    name = "Kam":
+    this_file = "ATBS_v10.py"
+    file_number = 10
+    
+    
+    if == this_file:
+        print("Correct")
+        break
+    else: continue
+    print("That is not correct")
+
+input("What is ")
+    if this_file != "ATBS_v10.py" and file_number >= 10:
+        name = "Kam"
+        print("These are my notes titled \"ATBS_v10")
+else: print("This is not the right file.")
+
+if input() == password:
+    print("Access Granted")
+
+"Kam_ATBS_v10.py_10":
 
 # QUESTION: 
 # ANSWER: 
